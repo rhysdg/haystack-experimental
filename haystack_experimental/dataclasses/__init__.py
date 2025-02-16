352e0737355750f5deb711f41799e869afbe82c7
Rhys Williams <rhys@pronti.ai>
2025-02-16 12:30:28 -0500

    core update

--- haystack_experimental/dataclasses/__init__.py
+++ haystack_experimental/dataclasses/__init__.py
@@ -2,12 +2,32 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
+from haystack_experimental.dataclasses.byte_stream import ByteStream
+from haystack_experimental.dataclasses.chat_message import (
+    ChatMessage,
+    ChatMessageContentT,
+    ChatRole,
+    MediaContent,
+    TextContent,
+    ToolCall,
+    ToolCallResult,
+)
 from haystack_experimental.dataclasses.streaming_chunk import (
     AsyncStreamingCallbackT,
     StreamingCallbackT,
 )
+from haystack_experimental.dataclasses.tool import Tool
 
 __all__ = [
     "AsyncStreamingCallbackT",
+    "ByteStream",
+    "ChatMessage",
+    "ChatMessageContentT",
+    "ChatRole",
+    "MediaContent",
     "StreamingCallbackT",
+    "TextContent",
+    "ToolCall",
+    "ToolCallResult",
+    "Tool",
 ]
