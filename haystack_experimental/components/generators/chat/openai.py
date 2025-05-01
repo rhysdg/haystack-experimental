0d1e7665fcf2d3c4515ccdd3282a6f35f01cf480
Rhys Williams <rhysdgwilliams@gmail.com>
2025-04-30 22:26:25 -0400

    resolving indentation issue

--- haystack_experimental/components/generators/chat/openai.py
+++ haystack_experimental/components/generators/chat/openai.py
@@ -49,8 +49,7 @@ def _convert_message_to_openai_format(message: ChatMessage) -> Dict[str, Any]:
     text_contents = message.texts
     tool_calls = message.tool_calls
     tool_call_results = message.tool_call_results
-
-     media_contents = [MediaContent(media=media) for media in message.media] if message.media else []
+    media_contents = [MediaContent(media=media) for media in message.media] if message.media else []
        
 
     if not text_contents and not tool_calls and not tool_call_results:
